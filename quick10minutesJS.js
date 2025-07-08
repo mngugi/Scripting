aa98afd121755aa1a1af210083a9b3b073acd9a9
P.M.Ngugi <mngugi7@gmail.com>
2025-07-09 00:21:56 +0300

    Update quick10minutesJS.js

---
+++ quick10minutesJS.js
@@ -0,0 +1,13 @@
+// Javascript Quick 10-Minute Guide
+// JavaScript is a versatile language used for web development, server-side programming, and more. This guide provides a quick overview of its key features and syntax.
+// Variables
+
+let num = 10; // Number
+// here num stores a number 10
+
+var age = 25; // Number
+// here age stores a number 25      
+let name = "John"; // String
+// here name stores a string "John"
+const PI = 3.14; // Constant
+// here PI is a constant with value 3.14
