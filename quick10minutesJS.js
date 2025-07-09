2912adac4764e49a5c40ea74ac0b31e78ba75c2d
P.M.Ngugi <mngugi7@gmail.com>
2025-07-09 21:24:47 +0300

    Update quick10minutesJS.js

--- quick10minutesJS.js
+++ quick10minutesJS.js
@@ -107,3 +107,29 @@ function studentInfor() {
     student.studentInfor = studentInfor;
     student.studentInfor();  
 console.log("\n--------\n")
+let x = 5;
+
+// addition operator
+console.log("Addition: x + 3 = ", x + 3);
+
+// subtraction operator
+console.log("Subtraction: x - 3 =", x - 3);
+
+// multiplication operator
+console.log("Multiplication: x * 3 =", x * 3);
+
+// division operator
+console.log("Division: x / 3 =", x / 3);
+
+// remainder operator
+console.log("Remainder: x % 3 =", x % 3);
+
+// increment operator
+console.log("Increment: ++x =", ++x);
+
+// decrement operator
+console.log("Decrement: --x =", --x);
+
+// exponentiation operator
+console.log("Exponentiation: x ** 3 =", x ** 3);
+console.log("\n--------\n")
