03347bd3137344e0cf97f1ffeba62d1c5a95247b
P.M.Ngugi <mngugi7@gmail.com>
2025-07-09 21:26:08 +0300

    Update quick10minutesJS.js

--- quick10minutesJS.js
+++ quick10minutesJS.js
@@ -133,3 +133,31 @@ console.log("Decrement: --x =", --x);
 // exponentiation operator
 console.log("Exponentiation: x ** 3 =", x ** 3);
 console.log("\n--------\n")
+// assignment operator
+let a = 7;
+console.log("Assignment: a = 7, a =", a);
+
+// addition assignment operator
+a += 5;  // a = a + 5
+console.log("Addition Assignment: a += 5, a =", a);
+
+// subtraction assignment operator
+a -= 5;  // a = a - 5
+console.log("Subtraction Assignment: a -= 5, a =", a);
+
+// multiplication assignment operator
+a *= 2;  // a = a * 2
+console.log("Multiplication Assignment: a *= 2, a =", a);
+
+// division assignment operator
+a /= 2;  // a = a / 2
+console.log("Division Assignment: a /= 2, a =", a);
+
+// remainder assignment operator
+a %= 2;  // a = a % 2
+console.log("Remainder Assignment: a %= 2, a =", a);
+
+// exponentiation assignment operator
+a **= 2;  // a = a**2
+console.log("Exponentiation Assignment: a **= 7, a =", a);
+console.log("\n--------\n")
