75ab80e11beab11c1f7990306ab3539293a152a7
P.M.Ngugi <mngugi7@gmail.com>
2025-07-09 21:29:20 +0300

    Update quick10minutesJS.js

--- quick10minutesJS.js
+++ quick10minutesJS.js
@@ -161,3 +161,27 @@ console.log("Remainder Assignment: a %= 2, a =", a);
 a **= 2;  // a = a**2
 console.log("Exponentiation Assignment: a **= 7, a =", a);
 console.log("\n--------\n")
+// equal to operator
+console.log("Equal to: 2 == 2 is", 2 == 2);
+
+// not equal operator
+console.log("Not equal to: 3 != 3 is", 3 != 3);
+
+// strictly equal to operator
+console.log("Strictly equal to: 2 === '2' is", 2 === '2');
+
+// strictly not equal to operator
+console.log("Strictly not equal to: 2 !== '2' is", 2 !== '2');
+
+// greater than operator
+console.log("Greater than: 3 > 3 is", 3 > 3);
+
+// less than operator
+console.log("Less than: 2 > 2 is", 2 > 2);
+
+// greater than or equal to operator
+console.log("Greater than or equal to: 3 >= 3 is", 3 >= 3);
+
+// less than or equal to operator
+console.log("Less than or equal to: 2 <= 2 is", 2 <= 2);
+console.log("\n--------\n")
