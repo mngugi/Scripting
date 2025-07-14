9799b16fc0be6ba1ebcf17a69db232d55fdaa370
P.M.Ngugi <mngugi7@gmail.com>
2025-07-14 20:55:01 +0300

    Update quick20minutesJS.js

--- quick20minutesJS.js
+++ quick20minutesJS.js
@@ -1,4 +1,13 @@
 // Data types
+// interger data type
 
 const Int_J = 54
 console.log("Integer:", Int_J, 'is a number:', typeof IntJ === 'number');
+
+// string data type
+
+const String_i = "The car could not start";
+console.log("String:", String_i, 'is a string:', typeof String_i === 'string');
+
+//  the type of function () { [native code] } is function
+
