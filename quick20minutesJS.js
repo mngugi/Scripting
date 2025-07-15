b911b5926dfac7ac8de74289d7198eb8bb50d2a5
P.M.Ngugi <mngugi7@gmail.com>
2025-07-15 12:31:49 +0300

    Update quick20minutesJS.js

--- quick20minutesJS.js
+++ quick20minutesJS.js
@@ -11,6 +11,7 @@ console.log("String:", String_i, 'is a string:', typeof String_i === 'string');
 
 //  the type of function () { [native code] } is function
 
+console.log("--------------------------------------------------");
 const newValues = [true, undefined, null, 0, NaN];
 for (let i = 0; i < newValues.length; i++) {
     console.log("Value:", newValues[i], 'is of type:', typeof newValues[i]);
