c605f593766f35d07b6d9e53e858eb63814d4888
P.M.Ngugi <mngugi7@gmail.com>
2025-07-15 14:14:04 +0300

    Update quick20minutesJS.js

--- quick20minutesJS.js
+++ quick20minutesJS.js
@@ -1,5 +1,5 @@
 // Data types
-// interger data type
+// integer data type
 console.log("--------------------------------------------------");
 const Int_J = 54
 console.log("Integer:", Int_J, 'is a number:', typeof IntJ === 'number');
