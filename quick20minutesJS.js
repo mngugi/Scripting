6af98dd54cd9c06c021421e019d0aaac054c7c51
P.M.Ngugi <mngugi7@gmail.com>
2025-07-15 13:08:12 +0300

    Update quick20minutesJS.js

--- quick20minutesJS.js
+++ quick20minutesJS.js
@@ -11,16 +11,26 @@ console.log("String:", String_i, 'is a string:', typeof String_i === 'string');
 
 //  the type of function () { [native code] } is function
 
-console.log("--------------------------------------------------");
+console.log("--------------------------------------------------------");
 const newValues = [true, undefined, null, 0, NaN];
 for (let i = 0; i < newValues.length; i++) {
     console.log("Value:", newValues[i], 'is of type:', typeof newValues[i]);
 } 
-console.log("------------------------Example--------------------------");
+console.log("------------------------Example-------------------------");
 
 const fruits = ["apple", "banana", "cherry"];
 for (let j= 0 ; j < fruits.length; j++) {
     console.log("Fruit:", fruits[j], 'is of type:', typeof fruits[j]);
 }
 
+console.log("--------------------------------------------------------");
+
+/* Control Flow */
+
+const nestedSports = [["football", "basketball"], ["tennis", "cricket"]];
+for (let i = 0; i < nestedSports.length; i++) {
+    for (let j = 0; j < nestedSports[i].length; j++) {
+        console.log("Sport:", nestedSports[i][j]);
+    }
+}
 console.log("--------------------------------------------------------");
