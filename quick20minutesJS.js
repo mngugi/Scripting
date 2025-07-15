463114fde49ff25766003d3a3db103dea055591d
P.M.Ngugi <mngugi7@gmail.com>
2025-07-15 12:32:30 +0300

    Update quick20minutesJS.js

--- quick20minutesJS.js
+++ quick20minutesJS.js
@@ -1,11 +1,11 @@
 // Data types
 // interger data type
-
+console.log("--------------------------------------------------");
 const Int_J = 54
 console.log("Integer:", Int_J, 'is a number:', typeof IntJ === 'number');
 
 // string data type
-
+console.log("--------------------------------------------------");
 const String_i = "The car could not start";
 console.log("String:", String_i, 'is a string:', typeof String_i === 'string');
 
@@ -16,3 +16,4 @@ const newValues = [true, undefined, null, 0, NaN];
 for (let i = 0; i < newValues.length; i++) {
     console.log("Value:", newValues[i], 'is of type:', typeof newValues[i]);
 } 
+console.log("--------------------------------------------------");
