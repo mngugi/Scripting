814cd18854866bdd39b653f4cbf53dbb80ed6470
P.M.Ngugi <mngugi7@gmail.com>
2025-07-16 22:44:50 +0300

    Update OofN.js

--- OofN.js
+++ OofN.js
@@ -1,6 +1,17 @@
+
+/*O(1) does not change with respect to input space. Hence, O(1) is referred
+to as being constant time. An example of an O(1) algorithm is accessing an
+item in the array by its index. O(n) is linear time and applies to  
+algorithms that must do n operations in the worst-case scenario. 
+An example of an O(n) algorithm is printing numbers from 0 to n-1,  
+as shown here: */
+
 function exampleLinear(n) {
     for (let i = 0; i < n; i++) {
         console.log("This is a linear time complexity example.", i);
     }
 }
-console.log("----------------------Example----------------------------");   
+console.log("--------------------------------------------------");   
+
+/** Similarly, O(n2) is quadratic time, and O(n3) is cubic time. 
+ * Examples  of these complexities are shown here: */
