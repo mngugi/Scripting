64cf820bb850b285ec7c636953feca270bcc141b
P.M.Ngugi <mngugi7@gmail.com>
2025-07-16 22:28:34 +0300

    Create OofN.js

---
+++ OofN.js
@@ -0,0 +1,6 @@
+function exampleLinear(n) {
+    for (let i = 0; i < n; i++) {
+        console.log("This is a linear time complexity example.", i);
+    }
+}
+console.log("----------------------Example----------------------------");   
