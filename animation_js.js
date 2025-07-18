ed17dc2b8c23320716aa48f153576fb1a1671a8e
P.M.Ngugi <mngugi7@gmail.com>
2025-07-18 20:53:46 +0300

    animation_js.js

--- animation_js.js
+++ animation_js.js
@@ -38,4 +38,20 @@
             this.classList.toggle("tap");
         });
     }
-    );     
+    );   
+    function toggleOnClick(selector, toggleClass) {
+    document.querySelectorAll(selector).forEach(el =>
+        el.addEventListener("click", function () {
+            this.classList.toggle(toggleClass);
+        })
+    );
+}
+
+toggleOnClick(".eye", "blink");
+toggleOnClick(".mouth", "smile");
+toggleOnClick(".body", "shake");
+toggleOnClick(".arm", "wave");
+toggleOnClick(".leg", "kick");
+toggleOnClick(".hat", "tip");
+toggleOnClick(".shoe", "tap");
+  
