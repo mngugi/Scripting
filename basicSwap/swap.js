90b93b952ff52f9c227c6dd0211e4935f8636493
P.M.Ngugi <mngugi7@gmail.com>
2025-07-19 21:47:09 +0300

    Update swap.js

--- basicSwap/swap.js
+++ basicSwap/swap.js
@@ -6,3 +6,12 @@ function swap(a,b) {
     
 }
 console.log("Swapping 5 and 10:", swap(5, 10));
+
+function swaparray(arr, i, j) {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
+let nums = [10, 20, 30];
+swaparray(nums, 0, 2);
+console.log("Swapping the array [10, 20, 30]",nums); // Output: [30, 20, 10]
+
