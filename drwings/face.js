0fee8bb82f5fcba0de665bff23f3c1cb25c720e0
P.M.Ngugi <mngugi7@gmail.com>
2025-07-19 17:14:36 +0300

    drwings/face.js

---
+++ drwings/face.js
@@ -0,0 +1,4 @@
+ellipse(200,200,100,100);
+// This file is part of the drawings project
+// It is subject to the license terms in the LICENSE file found in the top-level directory of this distribution and at          
+//  
