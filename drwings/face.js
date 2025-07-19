2406b2f87bf0db9a6783917600cc25224e83a6d1
P.M.Ngugi <mngugi7@gmail.com>
2025-07-19 17:20:10 +0300

    Update face.js

--- drwings/face.js
+++ drwings/face.js
@@ -1,3 +1,12 @@
+function setup() {
+  createCanvas(400, 400); // Sets up a 400x400 canvas
+}
+
+function draw() {
+  background(240); // Light gray background
+  ellipse(200, 200, 100, 100); // Draws a circle at the center
+}
+
 ellipse(200,200,100,100);
 // This file is part of the drawings project
 // It is subject to the license terms in the LICENSE file found in the top-level directory of this distribution and at          
