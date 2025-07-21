50a0850ba4ba1271d721f2a01e764e2f4f9353e1
P.M.Ngugi <mngugi7@gmail.com>
2025-07-21 10:32:49 +0300

    basicSwap/sumArray/script.js

--- basicSwap/sumArray/script.js
+++ basicSwap/sumArray/script.js
@@ -1,6 +1,7 @@
 function arraySum(arr){
+    console.log("Enter Array: ", arr);
     let sum = 0;
-    for (let i =0; i < arraySum.length ; i++){
+    for (let i =0; i < arr.length ; i++){
         sum += arr[i];
     }
     return sum;   
